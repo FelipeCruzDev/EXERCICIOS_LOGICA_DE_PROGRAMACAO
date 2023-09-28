036d69d2653fe2ec15d33778afaea0c30695d913
Felipe <104950263+FelipeCruzDev@users.noreply.github.com>
2023-09-28 09:10:45 -0300

    Update exercicio06.py

--- exercicio06.py
+++ exercicio06.py
@@ -1,8 +1,8 @@
 num = int (input("DIGITE UM NUMERO: "))
 
 if num ==10:
-    print("O NUMERO TEM QUE SER DIFERENTE DE 10")
+    print("É IGUAL A 10")
 elif num > 10:
     print("{} É MAIOR QUE 10".format(num))
-if num < 10:
+else:
     print("{} NÃO É MAIOR QUE 10".format(num))
