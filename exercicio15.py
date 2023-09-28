c217f7d142fd011d919aa6b29ae4f2706985a89b
Felipe <104950263+FelipeCruzDev@users.noreply.github.com>
2023-09-28 07:35:12 -0300

    Update exercicio15.py

--- exercicio15.py
+++ exercicio15.py
@@ -5,8 +5,10 @@ crescente'''
 
 num1 = int(input("INFORME UM VALOR: "))
 num2 = int(input("INFORME UM VALOR: "))
+while num1 == num2:
+    num2 = int(input("NUMERO IGUAL, INFORME UM VALOR: "))
 
 if num1 < num2:
-    print("{},{}".format(num1,num2))
+        print("{},{}".format(num1,num2))
 else:
     print("{},{}".format(num2,num1))
